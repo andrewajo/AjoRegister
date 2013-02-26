12e8435b00eda2c0d52702aba37685f7d02ed329
Andrew Joe <andrewajoe@gmail.com>
2013-02-26 18:27:07 -0500

    Change routes configuration

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -1,18 +1,16 @@
 require_dependency "ajo_register/application_controller"
-module AjoRegister
-  class RegistrationsController < Devise::RegistrationsController
-    def after_sign_up_path_for(resource)
-      '/registration_thank_you'
-    end
+class AjoRegister::RegistrationsController < Devise::RegistrationsController
+  def after_sign_up_path_for(resource)
+    '/registration_thank_you'
+  end
 
-    def index
-    end
+  def index
+  end
 
-    def new
-      super
-    end
+  def new
+    super
+  end
 
-    def thank_you
-    end
+  def thank_you
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AjoRegister::Engine.routes.draw do
-  devise_for :users, {:class_name => "AjoRegister::User", module: :devise}, :controllers => {:registrations => 'ajo_register/registrations'},
+  devise_for :users, {:class_name => "AjoRegister::User", module: :devise}, :controllers => {:registrations => 'ajo_register/registrations'}
 
   match '/registration_thank_you' => 'ajo_register/registrations#thank_you', :as => 'register_thank_you'
   devise_scope :user do
