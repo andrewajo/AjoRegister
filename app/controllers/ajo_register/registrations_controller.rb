bfc23f38dcd01ac311540c8a99ac0ce416a3d76b
Andrew Joe <andrewajoe@gmail.com>
2013-02-26 18:23:46 -0500

    Change routes configuration

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -1,16 +1,18 @@
 require_dependency "ajo_register/application_controller"
-class AjoRegister::RegistrationsController < Devise::RegistrationsController
-  def after_inactive_sign_up_path_for(resource)
-    '/registration_thank_you'
-  end
+module AjoRegister
+  class RegistrationsController < Devise::RegistrationsController
+    def after_sign_up_path_for(resource)
+      '/registration_thank_you'
+    end
 
-  def index
-  end
+    def index
+    end
 
-  def new
-    super
-  end
+    def new
+      super
+    end
 
-  def thank_you
+    def thank_you
+    end
   end
 end
