e64e426b5334c662bb5f4352bda1ac07ecf0ac5d
Andrew Joe <andrewajoe@gmail.com>
2013-02-26 18:12:49 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,13 @@ AjoRegister::Engine.routes.draw do
   end
 
   Rails.application.routes.draw do
-    devise_for :users, :class_name => "AjoRegister::User"
+    devise_for :users, :controllers => {:registrations => 'ajo_register/registrations'}, :class_name => "AjoRegister::User", module: :devise
 
+    match '/registration_thank_you' => 'ajo_register/registrations#thank_you', :as => 'register_thank_you'
     devise_scope :user do
-      match '/registration_thank_you' => 'ajo_register/registrations#thank_you', :as => 'register_thank_you'
       root :to => 'devise/registrations#new'
       authenticated :user do
-        root :to => "ajo_register/registrations#thank_you"
+        root :to => "ajo_register/registrationsd#thank_you"
       end
     end
 
