22dc2a2780322ee06c4d5a116165b76594b712ac
Andrew Joe <andrewajoe@gmail.com>
2013-02-28 01:23:04 -0500

    Change routes configuration

--- lib/ajo_register/engine.rb
+++ lib/ajo_register/engine.rb
@@ -1,5 +1,6 @@
 module AjoRegister
   class Engine < ::Rails::Engine
+    isolate_namespace AjoRegister
     config.autoload_paths << File.expand_path("../lib/generators", __FILE__)
     paths["config/locales"]
   end
