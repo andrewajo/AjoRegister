6a35a932bb45843a3af77494329ede95a7557631
Andrew Joe <andrewajoe@gmail.com>
2013-03-02 21:45:58 -0500

    Add recaptcha

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -17,9 +17,7 @@ class AjoRegister::RegistrationsController < Devise::RegistrationsController
     else
       build_resource
       clean_up_passwords(resource)
-      flash.now[:alert] = "There was an error with the recaptcha code below. Please re-enter the code."
-      flash.delete :recaptcha_error
-      render :new
+      resource.errors.add(:recaptcha, 'Not Valid')
     end
   end
 
