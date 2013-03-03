15584c33ee388f346e0592cd6f3dde092c203e2e
Andrew Joe <andrewajoe@gmail.com>
2013-03-02 21:43:19 -0500

    Add recaptcha

--- app/models/ajo_register/user.rb
+++ app/models/ajo_register/user.rb
@@ -8,7 +8,7 @@ module AjoRegister
   
     # Setup accessible (or protected) attributes for your model
     attr_accessible :email, :password, :password_confirmation, :remember_me
-    attr_accessible :age_of_consent, :city, :email_address, :first_name, :gender, :last_name, :mailing_address, :opt_in, :province, :rules_and_regulations, :year_of_birth, :password, :password_confirmation
+    attr_accessible :age_of_consent, :city, :email_address, :first_name, :gender, :last_name, :mailing_address, :opt_in, :province, :rules_and_regulations, :year_of_birth, :password, :password_confirmation, :date_of_birth
 
   end
 end
