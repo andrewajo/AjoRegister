41a608ecad7fde7b8383b47a95c50a0ddda9654d
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-04 17:47:34 -0500

    Add double optin field

--- app/controllers/ajo_register/confirmations_controller.rb
+++ app/controllers/ajo_register/confirmations_controller.rb
@@ -9,6 +9,17 @@ class AjoRegister::ConfirmationsController < Devise::ConfirmationsController
     super
   end
 
+  def show
+    self.resource = resource_class.confirm_by_token(params[:confirmation_token])
+    if resource.errors.empty?
+      set_flash_message(:notice, :confirmed) if is_navigational_format?
+      sign_in(resource_name, resource)
+      redirect_to main_app.double_opt_in_path
+    else
+      respond_with_navigational(resource.errors, :status => :unprocessable_entity){ render :new }
+    end
+  end
+
   def destroy
     super
   end
