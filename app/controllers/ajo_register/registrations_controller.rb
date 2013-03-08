fdab51d0b39166ed08d4e1ed4fa3ed3ec800c093
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-08 01:36:47 -0500

    add rules validation

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -14,7 +14,7 @@ class AjoRegister::RegistrationsController < Devise::RegistrationsController
       resource.valid?
       resource.errors.add(:recaptcha, "* Please try again")
       clean_up_passwords(resource)
-      respond_with resource, :location => :back
+      respond_with resource, :location => new_user_registration_path
     else
       flash.delete :recaptcha_error
       super
