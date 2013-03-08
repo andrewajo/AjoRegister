d54f61afd5f77466735bbfa24281810031806973
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-08 06:41:35 -0500

    fix password validation

--- app/controllers/ajo_register/passwords_controller.rb
+++ app/controllers/ajo_register/passwords_controller.rb
@@ -24,18 +24,7 @@ class AjoRegister::PasswordsController < Devise::PasswordsController
   end
 
   def update
-    self.resource = resource_class.reset_password_by_token(resource_params)
-
-    if resource.errors.empty?
-      resource.unlock_access! if unlockable?(resource)
-      flash_message = resource.active_for_authentication? ? :updated : :updated_not_active
-      set_flash_message(:notice, flash_message) if is_navigational_format?
-      sign_in(resource_name, resource)
-      respond_with resource, :location => after_sign_in_path_for(resource)
-    else
-      @errors = resource.errors
-      render :edit, :reset_password_token => resource.reset_password_token
-    end
+    super
   end
 
 end
