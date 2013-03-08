d2c9978150cde0b1db7ccf758f16c1a5c8324208
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-07 20:05:11 -0500

    Add after sign up path

--- app/controllers/ajo_register/passwords_controller.rb
+++ app/controllers/ajo_register/passwords_controller.rb
@@ -6,7 +6,13 @@ class AjoRegister::PasswordsController < Devise::PasswordsController
   end
 
   def create
-    super
+    self.resource = resource_class.send_reset_password_instructions(resource_params)
+
+    if successfully_sent?(resource)
+      respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
+    else
+      redirect_to request.fullpath
+    end
   end
 
   def destroy
