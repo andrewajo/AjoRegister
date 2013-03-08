5cd969fd68425f2dd05ac4dce588512d35447a23
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-07 22:08:11 -0500

    reset errors

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -8,16 +8,21 @@ class AjoRegister::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    if !verify_recaptcha
-      flash.delete :recaptcha_error
-      build_resource
-      resource.valid?
-      resource.errors.add(:base, "There was an error with the recaptcha code below. Please re-enter the code.")
-      clean_up_passwords(resource)
-      respond_with_navigational(resource) { render_with_scope :new }
+    build_resource
+
+    if resource.save
+      if resource.active_for_authentication?
+        set_flash_message :notice, :signed_up if is_navigational_format?
+        sign_up(resource_name, resource)
+        respond_with resource, :location => after_sign_up_path_for(resource)
+      else
+        set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+        expire_session_data_after_sign_in!
+        respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+      end
     else
-      flash.delete :recaptcha_error
-      super
+      clean_up_passwords resource
+      respond_with resource, :location => new_user_registration_path
     end
   end
 
