1bbdd81fc5a69c826e08dbaba050305fbc572d46
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-10 14:15:08 -0400

    fix password validation

--- app/controllers/ajo_register/registrations_controller.rb
+++ app/controllers/ajo_register/registrations_controller.rb
@@ -14,10 +14,31 @@ class AjoRegister::RegistrationsController < Devise::RegistrationsController
       resource.valid?
       resource.errors.add(:recaptcha, "* Please try again")
       clean_up_passwords(resource)
-      respond_with resource, :location => new_user_registration_path
+      if resource.source == 'facebook'
+        respond_with resource, :location => main_app.facebook_register_path
+      else
+        respond_with resource, :location => new_user_registration_path
+      end
     else
       flash.delete :recaptcha_error
-      super
+      build_resource
+      if resource.source == 'facebook'
+        skip_confirmation!
+      end
+      if resource.save
+        if resource.active_for_authentication?
+          set_flash_message :notice, :signed_up if is_navigational_format?
+          sign_up(resource_name, resource)
+          respond_with resource, :location => after_sign_up_path_for(resource)
+        else
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
+          expire_session_data_after_sign_in!
+          respond_with resource, :location => after_inactive_sign_up_path_for(resource)
+        end
+      else
+        clean_up_passwords resource
+        respond_with resource
+      end
     end
   end
 
