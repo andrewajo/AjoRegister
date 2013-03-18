b4564cc96cd5c0221ce1fcf3f1369b2548a05e8d
Andrew Joe <andrew@andrewjoecreative.com>
2013-03-18 18:28:35 -0400

    remove facebook required password

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 AjoRegister::Engine.routes.draw do
-  devise_for :users, {:class_name => "AjoRegister::User", :module => :devise, :controllers => {:registrations => 'ajo_register/registrations', :passwords => 'ajo_register/passwords', :sessions => 'ajo_register/sessions', :confirmations => 'ajo_register/confirmations'}}
+  scope ":locale" do
+    devise_for :users, {:class_name => "AjoRegister::User", :module => :devise, :controllers => {:registrations => 'ajo_register/registrations', :passwords => 'ajo_register/passwords', :sessions => 'ajo_register/sessions', :confirmations => 'ajo_register/confirmations'}}
 
-  match '/registration_thank_you' => 'ajo_register/registrations#thank_you', :as => 'register_thank_you'
+    match '/registration_thank_you' => 'ajo_register/registrations#thank_you', :as => 'register_thank_you'
 
-  devise_scope :user do
-    root :to => 'registrations#new'
+    devise_scope :user do
+      root :to => 'registrations#new'
+    end
   end
 end
